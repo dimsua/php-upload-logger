914668412814e9092b8df7bd79d9770a71f81200
dimsua <dims.main@gmail.com>
2017-11-17 12:07:02 +0200

    Easy switch from suhosin.upload.verification_script
    Remove unused logging, and functions, switch to suhosin verification script output: 1/0 instead +/-

--- ul.c
+++ ul.c
@@ -14,8 +14,6 @@
 #include <Zend/zend_virtual_cwd.h>
 #include <ext/standard/info.h>
 #include <ext/standard/file.h>
-#include <ext/standard/php_smart_string.h>
-#include <ext/standard/php_string.h>
 #include <main/rfc1867.h>
 #include <main/SAPI.h>
 #include <main/spprintf.h>
@@ -27,127 +25,10 @@ ZEND_DECLARE_MODULE_GLOBALS(uploadlogger);
 
 PHP_INI_BEGIN()
     STD_PHP_INI_BOOLEAN("ul.enabled",              "0",    PHP_INI_PERDIR, OnUpdateBool,   enabled,        zend_uploadlogger_globals, uploadlogger_globals)
-    STD_PHP_INI_ENTRY("ul.dir",                    "/tmp", PHP_INI_PERDIR, OnUpdateString, dir,            zend_uploadlogger_globals, uploadlogger_globals)
     STD_PHP_INI_ENTRY("ul.verification_script",    "",     PHP_INI_PERDIR, OnUpdateString, script,         zend_uploadlogger_globals, uploadlogger_globals)
-    STD_PHP_INI_BOOLEAN("ul.save_unclaimed_files", "0",    PHP_INI_SYSTEM, OnUpdateBool,   save_unclaimed, zend_uploadlogger_globals, uploadlogger_globals)
-    STD_PHP_INI_ENTRY("ul.unclaimed_files_dir",    "",     PHP_INI_SYSTEM, OnUpdateString, unclaimed_dir,  zend_uploadlogger_globals, uploadlogger_globals)
 PHP_INI_END()
 
-static char* get_filename()
-{
-    char* buf;
-    spprintf(&buf, 1024, "%s/%lu.log", UL_G(dir), (unsigned long)getuid());
-    return buf;
-}
-
-static void get_fileid(smart_string* s)
-{
-    unsigned long int pid = (unsigned long int)getpid();
-    unsigned long int ctr = (unsigned long int)UL_G(ctr);
-#ifdef ZTS
-    unsigned long int tid = (unsigned long int)tsrm_thread_id();
-#endif
-
-    smart_string_append_long(s, pid);
-    smart_string_appendc(s, '_');
-#ifdef ZTS
-    smart_string_append_long(s, tid);
-    smart_string_appendc(s, '_');
-#endif
-    smart_string_append_long(s, ctr);
-}
-
-static void get_remote_addr(smart_string* s)
-{
-    char* remote_addr          = NULL;
-    char* http_x_forwarded_for = NULL;
-    char* cf_connecting_ip     = NULL;
-    /// TODO: do I really need to estrdup() values?
-    if (sapi_module.getenv) {
-        char* tmp;
-        tmp = sapi_module.getenv(ZEND_STRL("REMOTE_ADDR"));
-        if (tmp) remote_addr = estrdup(tmp);
-        tmp = sapi_module.getenv(ZEND_STRL("HTTP_X_FORWARDED_FOR"));
-        if (tmp) http_x_forwarded_for = estrdup(tmp);
-        tmp = sapi_module.getenv(ZEND_STRL("HTTP_CF_CONNECTING_IP"));
-        if (tmp) cf_connecting_ip = estrdup(tmp);
-    }
-
-    if (!remote_addr && !PG(during_request_startup)) {
-        zval* server;
-        zval* v;
-
-        zend_is_auto_global_str(ZEND_STRL("_SERVER"));
-        server = &PG(http_globals)[TRACK_VARS_SERVER];
-
-        v = zend_hash_str_find(Z_ARRVAL_P(server), ZEND_STRL("REMOTE_ADDR"));
-        if (v && Z_TYPE_P(v) == IS_STRING) {
-            remote_addr = estrndup(Z_STRVAL_P(v), Z_STRLEN_P(v));
-        }
-
-        v = zend_hash_str_find(Z_ARRVAL_P(server), ZEND_STRL("HTTP_X_FORWARDED_FOR"));
-        if (v && Z_TYPE_P(v) == IS_STRING) {
-            http_x_forwarded_for = estrndup(Z_STRVAL_P(v), Z_STRLEN_P(v));
-        }
-
-        v = zend_hash_str_find(Z_ARRVAL_P(server), ZEND_STRL("HTTP_CF_CONNECTING_IP"));
-        if (v && Z_TYPE_P(v) == IS_STRING) {
-            cf_connecting_ip = estrndup(Z_STRVAL_P(v), Z_STRLEN_P(v));
-        }
-    }
-
-    if (!remote_addr) {
-        char* tmp;
-        tmp = getenv("REMOTE_ADDR");
-        if (tmp) remote_addr = estrdup(tmp);
-        tmp = getenv("HTTP_X_FORWARDED_FOR");
-        if (tmp) http_x_forwarded_for = estrdup(tmp);
-        tmp = getenv("HTTP_CF_CONNECTING_IP");
-        if (tmp) cf_connecting_ip = estrdup(tmp);
-    }
-
-    smart_string_appends(s, "REMOTE_ADDR: ");
-    smart_string_appends(s, remote_addr ? remote_addr : "N/A");
-    smart_string_appendc(s, '\n');
-
-    if (http_x_forwarded_for) {
-        smart_string_appends(s, "HTTP_X_FORWARDED_FOR: ");
-        smart_string_appends(s, http_x_forwarded_for ? http_x_forwarded_for : "N/A");
-        smart_string_appendc(s, '\n');
-    }
-
-    if (cf_connecting_ip) {
-        smart_string_appends(s, "HTTP_CF_CONNECTING_IP: ");
-        smart_string_appends(s, cf_connecting_ip ? cf_connecting_ip : "N/A");
-        smart_string_appendc(s, '\n');
-    }
-
-    efree(remote_addr);
-    efree(http_x_forwarded_for);
-    efree(cf_connecting_ip);
-}
-
-static void get_current_time(smart_string* s)
-{
-    char buf[32];
-    time_t t      = time(NULL);
-    struct tm* tm = localtime(&t);
-    size_t len    = strftime(buf, sizeof(buf), "[%Y-%m-%d %H:%M:%S]", tm);
-
-    smart_string_appendl(s, buf, len);
-}
-
-static ssize_t safe_write(int fd, const char* buf, size_t len)
-{
-    ssize_t res;
-    do {
-        res = write(fd, buf, len);
-    } while (-1 == res && (EAGAIN == errno || EINTR == errno));
-
-    return res;
-}
-
-static int verify_file(smart_string* out, const char* script, const char* file)
+static int verify_file(const char* script, const char* file)
 {
     FILE* f;
     int outcome;
@@ -187,7 +68,6 @@ static int verify_file(smart_string* out, const char* script, const char* file)
 
     {
         char c;
-        char buf[512];
         size_t n = fread(&c, 1, sizeof(c), f);
         if (!n) {
             pclose(f);
@@ -195,20 +75,11 @@ static int verify_file(smart_string* out, const char* script, const char* file)
             return FAILURE;
         }
 
-        if (c != '+') {
+        if (c != '1') {
             outcome = FAILURE;
-            smart_string_appends(out, "The file is disallowed by the verification script:\n");
-            smart_string_appendc(out, c);
-            while ((n = fread(buf, 1, 512, f)) != 0) {
-                smart_string_appendl(out, buf, n);
-            }
         }
         else {
             outcome = SUCCESS;
-            smart_string_appends(out, "The file is allowed by the verification script\n");
-            while ((n = fread(buf, 1, 512, f)) != 0) {
-                // Do nothing
-            }
         }
     }
 
@@ -218,125 +89,20 @@ static int verify_file(smart_string* out, const char* script, const char* file)
 
 static int my_rfc1867_callback(unsigned int event, void* event_data, void** extra)
 {
-    if (UL_G(enabled) && UL_G(dir)) {
-        if (MULTIPART_EVENT_FILE_START == event) {
-            multipart_event_file_start* data = (multipart_event_file_start*)event_data;
-            int fd = UL_G(fd);
-            char* filename;
-
-            if (fd != -1) {
-                close(fd);
-            }
-
-            filename = get_filename();
-            if (!filename) {
-                zend_error(E_WARNING, "Out of memory");
-                return FAILURE;
-            }
-
-            fd = open(filename, O_CREAT | O_APPEND | O_WRONLY, 0600);
-            if (fd == -1) {
-                zend_error(E_WARNING, "Failed to open file %s (%s)", filename, strerror(errno));
-                efree(filename);
-                return FAILURE;
-            }
-
-            efree(filename);
-            UL_G(fd) = fd;
-            ++UL_G(ctr);
-
-            {
-                smart_string s        = { NULL, 0, 0 };
-                char* filename        = *(data->filename);
-                char* path_translated = SG(request_info).path_translated;
-                char* query_string    = SG(request_info).query_string;
-                char* request_uri     = SG(request_info).request_uri;
-
-                get_current_time(&s);
-                smart_string_appends(&s, " File ID: ");
-                get_fileid(&s);
-                smart_string_appends(&s, "\nFilename: ");
-                smart_string_appends(&s, filename ? filename : "N/A");
-                smart_string_appends(&s, "\nREQUEST_URI: ");
-                smart_string_appends(&s, request_uri ? request_uri : "N/A");
-                smart_string_appends(&s, "\nPATH_TRANSLATED: ");
-                smart_string_appends(&s, path_translated ? path_translated : "N/A");
-                smart_string_appends(&s, "\nQUERY_STRING: ");
-                smart_string_appends(&s, query_string ? query_string : "N/A");
-                smart_string_appendc(&s, '\n');
-                get_remote_addr(&s);
-                smart_string_appends(&s, "\n\n");
-                smart_string_0(&s);
-
-                safe_write(fd, s.c, s.len);
-                smart_string_free(&s);
-            }
-        }
-        else if (MULTIPART_EVENT_FILE_END == event) {
-            int fd = UL_G(fd);
-            if (fd != -1) {
+    if UL_G(enabled) {
+        if (MULTIPART_EVENT_FILE_END == event) {
                 multipart_event_file_end* data = (multipart_event_file_end*)event_data;
-                smart_string s                 = { NULL, 0, 0 };
-                char* filename                 = data->temp_filename;
                 int fail                       = 0;
 
-                get_current_time(&s);
-                smart_string_appends(&s, " File ID: ");
-                get_fileid(&s);
-
-                /*
-                 * This is what PHP does:
-                 *      temp_filename = NULL;
-                 *
-                 *      // ...
-                 *
-                 *      if (filename[0] == '\0') {
-                 *          cancel_upload = UPLOAD_ERROR_D;
-                 *      }
-                 *
-                 *      // ...
-                 *
-                 *      if (!cancel_upload) {
-                 *          // ...
-                 *
-                 *          fd = php_open_temporary_fd_ex(PG(upload_tmp_dir), "php", &temp_filename, 1);
-                 *      }
-                 *
-                 *  Thus, for UPLOAD_ERROR_D ( == 4, "No file uploaded"), data->temp_filename
-                 *  will be garbage (ZSTR_VAL(NULL)).
-                 */
-                if (data->cancel_upload != 4) {
-                    smart_string_appends(&s, "\nTemporary filename: ");
-                    smart_string_appends(&s, filename ? filename : "N/A");
-                }
-
-                smart_string_appends(&s, "\nUpload status: ");
-                smart_string_append_long(&s, data->cancel_upload);
-                smart_string_appendc(&s, '\n');
-                smart_string_0(&s);
-
-                safe_write(fd, s.c, s.len);
-                s.len = 0;
-
                 if (!data->cancel_upload && UL_G(script) && EXPECTED(data->temp_filename)) {
-                    if (FAILURE == verify_file(&s, UL_G(script), data->temp_filename)) {
+                    if (FAILURE == verify_file(UL_G(script), data->temp_filename)) {
                         fail = 1;
                     }
                 }
 
-                smart_string_appends(&s, "\n\n");
-                smart_string_0(&s);
-
-                safe_write(fd, s.c, s.len);
-                smart_string_free(&s);
-                fsync(fd);
-                close(fd);
-                UL_G(fd) = -1;
-
                 if (fail) {
                     return FAILURE;
                 }
-            }
         }
     }
 
@@ -348,62 +114,21 @@ static PHP_FUNCTION(move_uploaded_file)
     assert(old_move_uploaded_file != NULL);
     old_move_uploaded_file(INTERNAL_FUNCTION_PARAM_PASSTHRU);
 
-    if (UL_G(enabled) && UL_G(dir)) {
-        int fd;
+    if UL_G(enabled) {
         char* path;
         char* new_path;
-        char* filename;
         size_t path_len, new_path_len;
 
         if (zend_parse_parameters(ZEND_NUM_ARGS(), "ss", &path, &path_len, &new_path, &new_path_len) == FAILURE) {
             return;
-        }
-
-        filename = get_filename();
-        if (!filename) {
-            return;
-        }
-
-        fd = open(filename, O_CREAT | O_APPEND | O_WRONLY, 0600);
-        if (fd == -1) {
-            zend_error(E_WARNING, "Failed to open file %s (%s)", filename, strerror(errno));
-            efree(filename);
-            return;
-        }
-
-        efree(filename);
-
-        {
-            smart_string s = { NULL, 0, 0 };
-            get_current_time(&s);
-            smart_string_appendc(&s, '\n');
-            get_remote_addr(&s);
-            smart_string_appends(&s, "move_uploaded_file: old=");
-            smart_string_appendl(&s, path, path_len);
-            smart_string_appends(&s, ", new=");
-            smart_string_appendl(&s, new_path, new_path_len);
-            smart_string_appends(&s, ", status=");
-            smart_string_appends(&s, zend_is_true(return_value) ? "SUCCESS" : "FAILURE");
-            smart_string_appends(&s, "\n\n\n");
-            smart_string_0(&s);
-
-            safe_write(fd, s.c, s.len);
-            smart_string_free(&s);
-            fsync(fd);
-            close(fd);
         }
     }
 }
 
 static PHP_GINIT_FUNCTION(uploadlogger)
 {
-    uploadlogger_globals->dir            = NULL;
     uploadlogger_globals->script         = NULL;
-    uploadlogger_globals->unclaimed_dir  = NULL;
-    uploadlogger_globals->ctr            = 0;
-    uploadlogger_globals->fd             = -1;
     uploadlogger_globals->enabled        = 0;
-    uploadlogger_globals->save_unclaimed = 0;
 }
 
 static PHP_MINIT_FUNCTION(uploadlogger)
@@ -432,10 +157,6 @@ static PHP_MSHUTDOWN_FUNCTION(uploadlogger)
         }
     }
 
-    if (UL_G(fd) != -1) {
-        close(UL_G(fd));
-    }
-
     UNREGISTER_INI_ENTRIES();
     return SUCCESS;
 }
@@ -450,110 +171,8 @@ static PHP_MINFO_FUNCTION(uploadlogger)
     DISPLAY_INI_ENTRIES();
 }
 
-static int save_unclaimed_file(zval* z)
-{
-    zend_string* filename = Z_STR_P(z);
-    char* target_dir      = UL_G(unclaimed_dir);
-    smart_string target   = { NULL, 0, 0 };
-    smart_string s        = { NULL, 0, 0 };
-
-    {
-        time_t now = time(NULL);
-        zend_string* fname_base = php_basename(ZSTR_VAL(filename), ZSTR_LEN(filename), NULL, 0);
-        smart_string_appends(&target, target_dir);
-        smart_string_appendc(&target, '/');
-        smart_string_appendl(&target, ZSTR_VAL(fname_base), ZSTR_LEN(fname_base));
-        smart_string_appendc(&target, '-');
-        smart_string_append_long(&target, (unsigned long)now);
-        smart_string_0(&target);
-        zend_string_release(fname_base);
-    }
-
-    if (VCWD_RENAME(ZSTR_VAL(filename), target.c) == 0) {
-        smart_string_appends(&s, "Moved unclaimed file ");
-        smart_string_appendl(&s, ZSTR_VAL(filename), ZSTR_LEN(filename));
-        smart_string_appends(&s, " to ");
-        smart_string_append(&s, &target);
-    }
-    else if (php_copy_file_ex(ZSTR_VAL(filename), target.c, STREAM_DISABLE_OPEN_BASEDIR) == SUCCESS) {
-        smart_string_appends(&s, "Copied unclaimed file ");
-        smart_string_appendl(&s, ZSTR_VAL(filename), ZSTR_LEN(filename));
-        smart_string_appends(&s, " to ");
-        smart_string_append(&s, &target);
-
-        VCWD_UNLINK(ZSTR_VAL(filename));
-    }
-    else {
-        smart_string_appends(&s, "Failed to handle unclaimed file ");
-        smart_string_appendl(&s, ZSTR_VAL(filename), ZSTR_LEN(filename));
-    }
-
-    smart_string_appendc(&s, '\n');
-    safe_write(UL_G(fd), s.c, s.len);
-    smart_string_free(&s);
-    smart_string_free(&target);
-    return ZEND_HASH_APPLY_KEEP;
-}
-
 static PHP_RSHUTDOWN_FUNCTION(uploadlogger)
 {
-    if (UL_G(save_unclaimed) && SG(rfc1867_uploaded_files) && zend_hash_num_elements(SG(rfc1867_uploaded_files)) > 0) {
-        char* target_dir = UL_G(unclaimed_dir);
-        int fd;
-
-        while (isspace(*target_dir)) {
-            ++target_dir;
-        }
-
-        if (!*target_dir) {
-            return SUCCESS;
-        }
-
-        {
-            struct stat st;
-            if (VCWD_STAT(target_dir, &st) < 0 || !S_ISDIR(st.st_mode)) {
-                zend_error(E_NOTICE, "ul.unclaimed_files_dir does not exist or not a directory");
-                return SUCCESS;
-            }
-        }
-
-        {
-            char* filename = get_filename();
-            if (UNEXPECTED(!filename)) {
-                return SUCCESS;
-            }
-
-            fd = open(filename, O_CREAT | O_APPEND | O_WRONLY, 0600);
-            if (fd == -1) {
-                zend_error(E_WARNING, "Failed to open file %s (%s)", filename, strerror(errno));
-                efree(filename);
-                return SUCCESS;
-            }
-
-            efree(filename);
-            UL_G(fd) = fd;
-        }
-
-        {
-            smart_string s = { NULL, 0, 0 };
-            get_current_time(&s);
-            smart_string_appends(&s, "\nSaving unclaimed files\n");
-            safe_write(fd, s.c, s.len);
-
-            UL_G(unclaimed_dir) = target_dir;
-            zend_hash_apply(SG(rfc1867_uploaded_files), save_unclaimed_file);
-
-            s.len = 0;
-            smart_string_appendl(&s, "\n\n", 2);
-            safe_write(fd, s.c, s.len);
-            smart_string_free(&s);
-        }
-
-        fsync(fd);
-        close(fd);
-        UL_G(fd) = -1;
-    }
-
     return SUCCESS;
 }
 
